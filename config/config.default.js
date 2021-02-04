08859e8bc25bf132ca6c55c791b4e0c45f58ec68
rmlzy <rmlzy@outlook.com>
2021-02-04 10:01:53 +0800

    feat: update theme

--- config/config.default.js
+++ config/config.default.js
@@ -18,8 +18,8 @@ module.exports = (appInfo) => {
   config.title = `${config.rmlzy} 💖 ${config.poppy}`;
   config.fallInLoveAt = "2013-07-20";
   config.subtitle =
-    "<p><a href='/blog/about-rmlzy.html'>远子</a>: 一只程序猿</p>" +
-    "<p><a href='/blog/about-poppy.html'>Poppy</a>: 一只产品汪</p>" +
+    "<p><a href='/blog/about-rmlzy.html'>远子</a>: 一只程序猿 🐒</p>" +
+    "<p><a href='/blog/about-poppy.html'>Poppy</a>: 一只产品汪 🐶</p>" +
     "<p>我们有一个可爱的宝宝~</p>";
   config.description = "远子和 Poppy 的小站, 记录一下关于开发、设计的文章";
   config.keywords = "poppy, rmlzy, 远子, 产品设计, 前端开发, 技术博客";
